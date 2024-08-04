1445320291e4ba0845df40f02b88306482d9c972
task-er <task.complex@gmail.com>
2024-08-05 03:11:55 +0900

    feat: GNB 추가

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,43 +1,51 @@
 import { Tabs } from 'expo-router';
 import React from 'react';
 
-import { TabBarIcon } from '@/components/navigation/TabBarIcon';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
+import TabBarIcon from '@/components/navigation/TabBarIcon';
+import { colors } from '@/theme';
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
         headerShown: false,
+        tabBarActiveTintColor: colors.primary,
+        tabBarInactiveTintColor: colors.gray[7],
+        tabBarStyle: {
+          height: 80,
+          paddingTop: 8,
+          paddingBottom: 16,
+        },
+        tabBarIconStyle: {
+          bottom: 0.5,
+        },
+        tabBarLabelStyle: {
+          fontSize: 12,
+          lineHeight: 16,
+          fontWeight: 500,
+        },
+        tabBarLabelPosition: 'below-icon',
       }}
     >
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Home',
-          tabBarIcon: ({ color, focused }) => <TabBarIcon name={focused ? 'home' : 'home-outline'} color={color} />,
+          title: '홈',
+          tabBarIcon: ({ color, focused }) => <TabBarIcon name={focused ? 'home' : 'home-outline'} />,
         }}
       />
       <Tabs.Screen
         name="explore"
         options={{
-          title: 'Explore',
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'code-slash' : 'code-slash-outline'} color={color} />
-          ),
+          title: '인증',
+          tabBarIcon: ({ color, focused }) => <TabBarIcon name={focused ? 'auth' : 'auth-outline'} />,
         }}
       />
       <Tabs.Screen
         name="typography"
         options={{
-          title: 'Typography',
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'code-slash' : 'code-slash-outline'} color={color} />
-          ),
+          title: '마이',
+          tabBarIcon: ({ color, focused }) => <TabBarIcon name={focused ? 'mypage' : 'mypage-outline'} />,
         }}
       />
     </Tabs>
--- components/navigation/TabBarIcon.tsx
+++ components/navigation/TabBarIcon.tsx
@@ -1,9 +1,30 @@
-// You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
-
-import Ionicons from '@expo/vector-icons/Ionicons';
-import { type IconProps } from '@expo/vector-icons/build/createIconSet';
 import { type ComponentProps } from 'react';
+import { colors } from '@/theme';
+import HomeIcon from '@/assets/images/HomeIcon';
+import AuthIcon from '@/assets/images/auth.svg';
+import MyIcon from '@/assets/images/my.svg';
+import MyOutlineIcon from '@/assets/images/my-outline.svg';
+import AuthOutlineIcon from '@/assets/images/auth-outline.svg';
 
-export function TabBarIcon({ style, ...rest }: IconProps<ComponentProps<typeof Ionicons>['name']>) {
-  return <Ionicons size={28} style={[{ marginBottom: -3 }, style]} {...rest} />;
+interface Icons {
+  name: 'home' | 'home-outline' | 'auth' | 'auth-outline' | 'mypage' | 'mypage-outline';
 }
+function TabBarIcon({ name, ...rest }: ComponentProps<typeof HomeIcon> & Icons) {
+  switch (name) {
+    case 'home':
+      return <HomeIcon fill={colors.primary} stroke={colors.primary} {...rest} />;
+    case 'home-outline':
+      return <HomeIcon fill={colors.white} stroke={colors.gray[7]} {...rest} />;
+    case 'auth':
+      return <AuthIcon {...rest} />;
+    case 'auth-outline':
+      return <AuthOutlineIcon {...rest} />;
+    case 'mypage':
+      return <MyIcon {...rest} />;
+    case 'mypage-outline':
+      return <MyOutlineIcon {...rest} />;
+  }
+  return null;
+}
+
+export default TabBarIcon;
