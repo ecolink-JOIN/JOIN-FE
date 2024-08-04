e138e43fde8e812247ee5a5eb2ddcb24f3b30d43
task-er <task.complex@gmail.com>
2024-08-04 19:50:28 +0900

    fix: color -> react-native타입으로 변경

--- theme/colors/types.ts
+++ theme/colors/types.ts
@@ -1,5 +1,25 @@
-export type ColorHex = `#${string}`;
-export type ColorSteps = Record<'1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' | '10' | '11', ColorHex>;
-export interface ColorTypes {
-  [key: string]: ColorHex | ColorSteps;
+import { ColorValue } from 'react-native';
+
+export type ColorSteps = Record<'1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' | '10' | '11', ColorValue>;
+
+interface ColorStepTypes {
+  gray: ColorSteps;
+  red: ColorSteps;
+  orange: ColorSteps;
+  yellow: ColorSteps;
+  green: ColorSteps;
+  blue: ColorSteps;
+  purple: ColorSteps;
+  pink: ColorSteps;
 }
+
+interface ColorValueTypes {
+  primary: ColorValue;
+  sub1: ColorValue;
+  sub2: ColorValue;
+  black: ColorValue;
+  white: ColorValue;
+}
+
+// Combine both interfaces
+export type ColorTypes = ColorStepTypes & ColorValueTypes;
