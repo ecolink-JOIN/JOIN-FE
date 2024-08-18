bc10883dc3e6e312f3e1a43507b1ea517858d635
PROMLEE <donghoon099@naver.com>
2024-08-18 21:12:54 +0900

    feat: Add CustomDatePicker component

--- components/molecules/FormControl/RecruitBase/EndDate.tsx
+++ components/molecules/FormControl/RecruitBase/EndDate.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
-import { View, Text } from 'react-native';
-
-export const EndDate = () => {
+import Typography from '@/components/atoms/Typography';
+import FormModView from '@/components/atoms/View/FormMods';
+import { DatePicker } from '@/components/atoms/DatePicker';
+import dayjs from 'dayjs';
+import { BottomSheetModal } from '@gorhom/bottom-sheet';
+interface EndDateProps {
+  value: dayjs.Dayjs | null;
+  setValue: any;
+  bottomSheetModalRef: React.RefObject<BottomSheetModal>;
+}
+export const EndDate: React.FC<EndDateProps> = ({ value, setValue, bottomSheetModalRef }) => {
   return (
-    <View>
-      <Text>EndDate</Text>
-    </View>
+    <FormModView>
+      <Typography variant="button">모집 종료일</Typography>
+      <DatePicker placeholder="종료 일자" value={value} setValue={setValue} bottomSheetModalRef={bottomSheetModalRef} />
+    </FormModView>
   );
 };
