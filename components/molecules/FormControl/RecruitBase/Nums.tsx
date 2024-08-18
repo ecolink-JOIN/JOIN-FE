fcd02f95563474ec3faf4b17e9d08ab9242a12f7
PROMLEE <donghoon099@naver.com>
2024-08-18 17:59:59 +0900

    feat: CustomStepper 컴포넌트 추가

--- components/molecules/FormControl/RecruitBase/Nums.tsx
+++ components/molecules/FormControl/RecruitBase/Nums.tsx
@@ -1,10 +1,25 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
+import FormModView from '@/components/atoms/View/FormMods';
+import Typography from '@/components/atoms/Typography';
+import { Radio } from '@/components/atoms/Radio';
+import { Stepper } from '@/components/atoms/Stepper';
 
 export const Nums = () => {
+  const [selected, setSelected] = React.useState(false);
+  const [step, setStep] = React.useState(1);
   return (
     <View>
-      <Text>Nums</Text>
+      <FormModView>
+        <Typography variant="button">모집 인원</Typography>
+        <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+          <Radio selected={selected} onSelect={setSelected} />
+          <Typography variant="body2" style={{ marginLeft: 8 }}>
+            모집 인원 미정
+          </Typography>
+        </View>
+      </FormModView>
+      <Stepper active={!selected} step={step} stepConverter={setStep} />
     </View>
   );
 };
