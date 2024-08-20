cae12cf960448f0b06b94cecd8f4a26d62603371
task-er <task.complex@gmail.com>
2024-08-21 02:00:26 +0900

    feat: 신청하기 버튼 연결

--- app/(tabs)/(home)/(study)/_layout.tsx
+++ app/(tabs)/(home)/(study)/_layout.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import { Stack } from 'expo-router';
 import { View } from 'react-native';
-import { colors } from '@/theme';
 
 function StudyLayout() {
   return (
-    <View style={{ flex: 1, backgroundColor: colors.red[1] }}>
+    <View style={{ flex: 1 }}>
       <Stack screenOptions={{ headerShown: false }} />
     </View>
   );
--- components/organisms/StudyDetails/StudyApplicationSection/index.tsx
+++ components/organisms/StudyDetails/StudyApplicationSection/index.tsx
@@ -1,8 +1,16 @@
 import { View } from 'react-native';
 import StudyInfoSection from '@/components/molecules/StudyInfoSection';
 import Button from '@/components/atoms/Button';
+import { useRouter } from 'expo-router';
 
 const StudyApplicationSection: React.FC = () => {
+  const router = useRouter();
+  const studyId = 0;
+
+  const handlePress = () => {
+    router.push(`/(tabs)/(home)/(study)/${studyId}/application`);
+  };
+
   return (
     <>
       <View style={{ paddingHorizontal: 28, gap: 10 }}>
@@ -14,7 +22,7 @@ const StudyApplicationSection: React.FC = () => {
           padding: 20,
         }}
       >
-        <Button variant="contained" fullWidth size="large">
+        <Button variant="contained" fullWidth size="large" onPress={handlePress}>
           신청하기
         </Button>
       </View>
