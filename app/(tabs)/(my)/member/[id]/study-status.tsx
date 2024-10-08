80b1d34185dd21725424e530056bc2d6faff114e
task-er <task.complex@gmail.com>
2024-10-08 17:08:11 +0900

    feat: 스터디 스케쥴 퍼블리싱

--- app/(tabs)/(my)/member/[id]/study-status.tsx
+++ app/(tabs)/(my)/member/[id]/study-status.tsx
@@ -9,105 +9,103 @@ import {
   MyAttendance,
   StudySchedule,
 } from '@/components/organisms/MyPage/Manage';
-import { FlatList, View } from 'react-native';
+import { FlatList } from 'react-native';
 import { BottomSheetModalMethods } from '@gorhom/bottom-sheet/lib/typescript/types';
-import { BottomSheetModal } from '@gorhom/bottom-sheet';
-import Button from '@/components/atoms/Button';
 import BottomSheetComp from '@/components/molecules/BottomSheet';
-import TextField from '@/components/atoms/TextField';
-import { ModalWrapper } from '@/components/molecules/ModalViews';
 import styled from 'styled-components/native';
 import { colors } from '@/theme';
+import DateTimePicker from 'react-native-ui-datepicker';
 
-const StudyStatus = ({
-  id,
-  bottomSheetModalRef,
-}: {
-  id: string | string[] | undefined;
-  bottomSheetModalRef: React.RefObject<BottomSheetModalMethods>;
-}) => {
-  const [kakaolink, setKakaoLink] = React.useState('https://open.kakao.com/o/joinjoinjoi');
+const StudyStatus = ({ id }: { id: string | string[] | undefined }) => {
   const [isModalVisible, setIsModalVisible] = React.useState(false);
+  const selectedDates = [
+    '2024-10-08',
+    '2024-10-10',
+    '2024-10-15',
+    '2024-10-17',
+    '2024-10-22',
+    '2024-10-24',
+    '2024-10-29',
+    '2024-10-31',
+    '2024-11-05',
+    '2024-11-07',
+    '2024-11-12',
+    '2024-11-14',
+    '2024-11-19',
+    '2024-11-21',
+    '2024-11-26',
+    '2024-11-28',
+    '2024-12-03',
+    '2024-12-05',
+  ];
+
+  const bottomSheetModalRef = useRef<BottomSheetModalMethods>(null);
 
   const toggleModal = () => {
     setIsModalVisible(!isModalVisible);
   };
 
+  const handlePresentM = () => {
+    bottomSheetModalRef.current?.present();
+  };
+
   const _id = '123';
 
   return (
-    <ManageView>
-      <Typography variant="heading3">스터디 현황</Typography>
-      <ManageBox title="스터디 공지" icon="arrow-right" onPress={() => router.push(`member/${_id}/notice` as Href)}>
-        <StudyAnnouncement />
-      </ManageBox>
-      <ManageBox title="나의 출석 및 인증 현황">
-        <MyAttendance id={_id} />
-      </ManageBox>
-      <ManageBox title="스터디 출석 및 인증 현황">
-        <Attendance />
-      </ManageBox>
-      <ManageBox title="스터디 스케쥴" icon="arrow-right">
-        <StudySchedule />
-      </ManageBox>
+    <>
+      <ManageView>
+        <Typography variant="heading3">스터디 현황</Typography>
+        <ManageBox title="스터디 공지" icon="arrow-right" onPress={() => router.push(`member/${_id}/notice` as Href)}>
+          <StudyAnnouncement />
+        </ManageBox>
+        <ManageBox title="나의 출석 및 인증 현황">
+          <MyAttendance id={_id} />
+        </ManageBox>
+        <ManageBox title="스터디 출석 및 인증 현황">
+          <Attendance />
+        </ManageBox>
+        <ManageBox title="스터디 스케쥴" icon="arrow-right" onPress={handlePresentM}>
+          <StudySchedule />
+        </ManageBox>
 
-      <ManageBox
-        title="스터디 카카오톡 링크"
-        icon="pencil"
-        onPress={() => {
-          bottomSheetModalRef.current?.present();
-        }}
-      >
-        <KakaoLink />
-      </ManageBox>
-      <ManageBox>
-        <ListComponent title="스터디 탈퇴" />
-      </ManageBox>
+        <ManageBox title="스터디 카카오톡 링크">
+          <KakaoLink />
+        </ManageBox>
+        <ManageBox>
+          <ListComponent title="스터디 탈퇴" />
+        </ManageBox>
+      </ManageView>
+
       <BottomSheetComp
         bottomSheetModalRef={bottomSheetModalRef}
+        snapPoints={['55%']}
         component={
-          <View style={{ padding: 20, gap: 12 }}>
-            <Typography variant="subtitle1">스터디 카카오톡 링크 수정</Typography>
-            <TextField placeholder="카카오톡 링크를 입력해주세요." value={kakaolink} onChangeText={setKakaoLink} />
-            <Button
-              variant="contained"
-              style={{ marginHorizontal: 'auto' }}
-              onPress={() => bottomSheetModalRef.current?.dismiss()}
-            >
-              완료
-            </Button>
-          </View>
+          <DateView>
+            <DateTimePicker
+              mode="multiple"
+              locale="ko"
+              calendarTextStyle={{ fontFamily: 'Pretendard-Medium' }}
+              headerButtonColor={colors.primary}
+              selectedItemColor={colors.primary}
+              dates={selectedDates}
+            />
+          </DateView>
         }
       />
-      <ModalWrapper isModalVisible={isModalVisible} toggleModal={toggleModal}>
-        <ModalContents>
-          <Typography variant="subtitle1">스터디 종료하기</Typography>
-          <Typography variant="body3" style={{ color: colors.primary }}>
-            2024.06.04 - 2024.10.31
-          </Typography>
-          <Typography variant="body4" style={{ textAlign: 'center' }}>
-            설정된 스터디 기간이 남아있습니다.{'\n'}정말 종료하시겠습니까?
-          </Typography>
-          <Button variant="contained" onPress={toggleModal} style={{ marginHorizontal: 'auto' }}>
-            종료하기
-          </Button>
-        </ModalContents>
-      </ModalWrapper>
-    </ManageView>
+    </>
   );
 };
 
 const StudyStatusWrapper = () => {
   const { id } = useLocalSearchParams();
-  const bottomSheetModalRef = useRef<BottomSheetModal>(null);
-  return <FlatList data={[null]} renderItem={() => <StudyStatus {...{ id, bottomSheetModalRef }} />} />;
+
+  return <FlatList data={[null]} renderItem={() => <StudyStatus id={id} />} />;
 };
 
 export default StudyStatusWrapper;
 
-const ModalContents = styled.View`
-  gap: 12px;
-  justify-content: center;
+const DateView = styled.View`
   align-items: center;
-  padding: 32px;
+  justify-content: space-between;
+  padding: 8px;
 `;
